2405b4b9bb98d0cfc0fba58216de38ddf02c612b
Rannar Allorg <rannarallorg@gmail.com>
2017-07-06 21:57:25 +0300

    Add angular-material and basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {McloudTest} from "./mcloud-test/mcloud-test.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    McloudTest,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
